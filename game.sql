e0f0ff870786128ba15ca76b858d8531198f9476
Fredrik Lahode <lapi@MacBook-Pro-de-Fredrik.local>
2021-01-22 13:06:08 +0100

    Update database

--- game.sql
+++ game.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : maria_db:3306
--- Généré le : ven. 22 jan. 2021 à 11:12
+-- Généré le : ven. 22 jan. 2021 à 12:04
 -- Version du serveur :  10.5.8-MariaDB-1:10.5.8+maria~focal
 -- Version de PHP : 7.4.11
 
@@ -38,6 +38,32 @@ CREATE TABLE `player` (
   `gold` int(5) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `playerweapon`
+--
+
+CREATE TABLE `playerweapon` (
+  `idplayer` int(11) NOT NULL,
+  `idweapon` int(11) NOT NULL,
+  `equiped` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `weapon`
+--
+
+CREATE TABLE `weapon` (
+  `id` int(11) NOT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `str` int(2) NOT NULL,
+  `sta` int(2) NOT NULL,
+  `price` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Index pour les tables déchargées
 --
@@ -50,6 +76,20 @@ ALTER TABLE `player`
   ADD UNIQUE KEY `name` (`name`);
 
 --
+-- Index pour la table `playerweapon`
+--
+ALTER TABLE `playerweapon`
+  ADD PRIMARY KEY (`idplayer`,`idweapon`),
+  ADD KEY `idweapon` (`idweapon`),
+  ADD KEY `idplayer` (`idplayer`);
+
+--
+-- Index pour la table `weapon`
+--
+ALTER TABLE `weapon`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -58,6 +98,23 @@ ALTER TABLE `player`
 --
 ALTER TABLE `player`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `weapon`
+--
+ALTER TABLE `weapon`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `playerweapon`
+--
+ALTER TABLE `playerweapon`
+  ADD CONSTRAINT `idplayer` FOREIGN KEY (`idplayer`) REFERENCES `player` (`id`),
+  ADD CONSTRAINT `idweapon` FOREIGN KEY (`idweapon`) REFERENCES `weapon` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
